474e686a574a9abfee0e6e040480e02ea9e1e4db
El attari Yassine <yassineattari64@gmail.com>
2020-07-27 17:15:11 +0100

    feat: building register form

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -1,11 +1,82 @@
-import React from 'react';
+import React from "react";
+import {
+  Grid,
+  Form,
+  Segment,
+  Button,
+  Header,
+  Message,
+  Icon
+} from "semantic-ui-react";
+import { Link } from "react-router-dom";
 
 class Register extends React.Component {
-    render(){
-        return(
-            <div>Register</div>
-        );
-    }
+  state = {};
+
+  handleChange = () => {};
+
+  render() {
+    return (
+      <Grid textAlign="center" verticalAlign="middle" className="app">
+        <Grid.Column style={{ maxWidth: 450 }}>
+          <Header as="h2" icon color="blue" textAlign="center">
+            <Icon name="code" color="blue" />
+            Register for DevChat
+          </Header>
+          <Form size="large">
+            <Segment stacked>
+              <Form.Input
+                fluid
+                name="username"
+                icon="user"
+                iconPosition="left"
+                placeholder="Username"
+                onChange={this.handleChange}
+                type="text"
+              />
+
+              <Form.Input
+                fluid
+                name="email"
+                icon="mail"
+                iconPosition="left"
+                placeholder="Email Address"
+                onChange={this.handleChange}
+                type="email"
+              />
+
+              <Form.Input
+                fluid
+                name="password"
+                icon="lock"
+                iconPosition="left"
+                placeholder="Password"
+                onChange={this.handleChange}
+                type="password"
+              />
+
+              <Form.Input
+                fluid
+                name="passwordConfirmation"
+                icon="repeat"
+                iconPosition="left"
+                placeholder="Password Confirmation"
+                onChange={this.handleChange}
+                type="password"
+              />
+
+              <Button color="blue" fluid size="large">
+                Submit
+              </Button>
+            </Segment>
+          </Form>
+          <Message>
+            Already a user? <Link to="/login">Login</Link>
+          </Message>
+        </Grid.Column>
+      </Grid>
+    );
+  }
 }
 
 export default Register;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Login from './components/Auth/Login';
 import Register from './components/Auth/Register';
+
+import 'semantic-ui-css/semantic.min.css';
 
 const Root = () => (
     <Router>
