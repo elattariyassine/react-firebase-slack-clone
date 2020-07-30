30820f3ba892c194417dcbdb6b9b68246e454590
El attari Yassine <yassineattari64@gmail.com>
2020-07-30 18:05:45 +0100

    feat: Add Avatar to User Dropdown

--- src/components/SidePanel/UserPanel.js
+++ src/components/SidePanel/UserPanel.js
@@ -1,6 +1,6 @@
 import React from "react";
 import firebase from "../../firebase";
-import { Grid, Header, Icon, Dropdown } from "semantic-ui-react";
+import { Grid, Header, Icon, Dropdown, Image } from "semantic-ui-react";
 
 class UserPanel extends React.Component {
   state = {
@@ -35,6 +35,9 @@ class UserPanel extends React.Component {
   };
 
   render() {
+
+    const { user } = this.state;
+
     return (
       <Grid style={{ background: "#4c3c4c" }}>
         <Grid.Column>
@@ -44,15 +47,19 @@ class UserPanel extends React.Component {
               <Icon name="code" />
               <Header.Content>DevChat</Header.Content>
             </Header>
+            
+            {/* User Dropdown  */}
+            <Header style={{ padding: "0.25em" }} as="h4" inverted>
+              <Dropdown
+                trigger={<span>
+                          <Image src={user.photoURL} spaced="right" avatar/>
+                          {user.displayName}
+                        </span>}
+                options={this.dropdownOptions()}
+              />
+            </Header>
           </Grid.Row>
 
-          {/* User Dropdown  */}
-          <Header style={{ padding: "0.25em" }} as="h4" inverted>
-            <Dropdown
-              trigger={<span>{this.state.user.displayName}</span>}
-              options={this.dropdownOptions()}
-            />
-          </Header>
         </Grid.Column>
       </Grid>
     );
