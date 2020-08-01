fbb47fc390f15ec2b1eb557a8a48dde27f73535b
El attari Yassine <yassineattari64@gmail.com>
2020-08-01 14:18:29 +0100

    feat: Add channels components & channels creation modal & create new channels in the collection in the db

--- src/components/SidePanel/SidePanel.js
+++ src/components/SidePanel/SidePanel.js
@@ -1,6 +1,7 @@
 import React from "react";
 import UserPanel from "./UserPanel";
 import { Menu } from "semantic-ui-react";
+import Channels from './Channels';
 
 class SidePanel extends React.Component {
   render() {
@@ -15,6 +16,7 @@ class SidePanel extends React.Component {
         style={{ background: "#4c3c4c", fontSize: "1.2rem" }}
       >
         <UserPanel currentUser={currentUser} />
+        <Channels currentUser={currentUser} />
       </Menu>
     );
   }
