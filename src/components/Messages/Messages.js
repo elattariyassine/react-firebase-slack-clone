c30118657dc0c33bb803d454968b52c264f23b75
El attari Yassine <yassineattari64@gmail.com>
2020-08-02 01:40:56 +0100

    feat: creating colorPanel & messages & sending messages

--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -1,8 +1,35 @@
 import React from "react";
+import { Segment, Comment } from "semantic-ui-react";
+import firebase from "../../firebase";
+
+import MessagesHeader from "./MessagesHeader";
+import MessageForm from "./MessageForm";
 
 class Messages extends React.Component {
+  state = {
+    messagesRef: firebase.database().ref("messages"),
+    channel: this.props.currentChannel,
+    user: this.props.currentUser
+  };
+
   render() {
-    return <div>Messages</div>;
+    const { messagesRef, channel, user } = this.state;
+
+    return (
+      <React.Fragment>
+        <MessagesHeader />
+
+        <Segment>
+          <Comment.Group className="messages">{/* Messages */}</Comment.Group>
+        </Segment>
+
+        <MessageForm
+          messagesRef={messagesRef}
+          currentChannel={channel}
+          currentUser={user}
+        />
+      </React.Fragment>
+    );
   }
 }
 
